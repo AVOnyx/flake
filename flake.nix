24eca5898b43937b18839fd9e6c88399a231e2f5
AVOnyx <avonyx@proton.me>
2024-05-14 13:43:58 -0600

    Fixed flake formatting

--- flake.nix
+++ flake.nix
@@ -1,16 +1,14 @@
 {
-  description = "A very basic flake";
+  description = "AVOnyx's system flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
     nixosConfigurations.complex = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
   };
- 
 }
